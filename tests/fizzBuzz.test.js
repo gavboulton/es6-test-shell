b51c1ebbba5c05098fe9889d1f2218bc073a317a
Gavin Boulton <gavboulton@gmail.com>
2017-08-03 23:50:14 +0100

    Add fizz buzz step 2

--- tests/fizzBuzz.test.js
+++ tests/fizzBuzz.test.js
@@ -2,7 +2,9 @@ const fizzBuzz = (start, end) => {
   const arr = [];
 
   for (let i = start; i <= end; i += 1) {
-    if (i % 15 === 0) {
+    if (i.toString().match(/3/)) {
+      arr.push('lucky');
+    } else if (i % 15 === 0) {
       arr.push('fizzbuzz');
     } else if (i % 5 === 0) {
       arr.push('buzz');
@@ -21,9 +23,9 @@ describe('fizz buzz', () => {
     expect(typeof fizzBuzz).toBe('function');
   });
 
-  it('fizzes, buzzes and fizzbuzzes', () => {
-    const mapped = [1, 2, 'fizz', 4, 'buzz', 'fizz', 7, 8, 'fizz', 'buzz', 11, 'fizz', 13, 14, 'fizzbuzz', 16, 17, 'fizz', 19, 'buzz'];
-    const mappedString = '1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz 16 17 fizz 19 buzz';
+  it('fizzes, buzzes, fizzbuzzes and luckies', () => {
+    const mapped = [1, 2, 'lucky', 4, 'buzz', 'fizz', 7, 8, 'fizz', 'buzz', 11, 'fizz', 'lucky', 14, 'fizzbuzz', 16, 17, 'fizz', 19, 'buzz'];
+    const mappedString = '1 2 lucky 4 buzz fizz 7 8 fizz buzz 11 fizz lucky 14 fizzbuzz 16 17 fizz 19 buzz';
 
     expect(fizzBuzz(1, 20)).toEqual(mapped);
     expect(fizzBuzz(1, 20).join(' ')).toEqual(mappedString);
